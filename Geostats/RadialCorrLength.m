e3555b65a1cb270d70f533976ff59d753657dc44
leandro <leandro@ltrace.com.br>
2023-09-20 19:50:57 -0300

    Revert "2D anisotropic covariogram"
    This reverts commit dc5cf2692b352eed8af2aaab36cc56eff347be14.

--- Geostats/RadialCorrLength.m
+++ Geostats/RadialCorrLength.m
@@ -1,5 +1,4 @@
-function l = RadialCorrLength(l1, l2, l3, angles, theta)
-%function l = RadialCorrLength(lmin, lmax, azim, theta)
+function l = RadialCorrLength(lmin, lmax, azim, theta)
 
 % RADIAL CORR LENGTH computes the radial correlation length 
 % INPUT lmin = minimum correlation length
@@ -11,8 +10,4 @@ function l = RadialCorrLength(l1, l2, l3, angles, theta)
 % Written by Dario Grana (August, 2020)
 
 % covariance function
-l = sqrt( (lmin^2*lmax^2) ./ (lmax^2*(sin(azim-theta)).^2 + lmin^2*(cos(azim-theta)).^2) );
-
-%azim = angles(1);
-%dip = angles(2);
-%l = sqrt( (l1^2*l2^2*l3^2) ./ ( l3^2*(l2^2*(cos(azim-theta)).^2 + l1^2*(sin(azim-theta)).^2)*(cos(dip))^2 + l1^2*l2^2*sin(dip)^2 ) );
+l = sqrt((lmin^2*lmax^2)./(lmax^2*(sin(azim-theta)).^2+lmin^2*(cos(azim-theta)).^2));
